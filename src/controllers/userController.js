2b4a16eab3dc7b1ba3bb8672af2d514873bfba9d
subrat50 <subrats400@gmail.com>
2022-06-15 12:56:36 +0530

    b

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,14 +1,14 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
-const createUser = async function (abcd, xyz) {
+const createUser = async function (req, res) {
   //You can name the req, res objects anything.
   //but the first parameter is always the request 
   //the second parameter is always the response
-  let data = abcd.body;
+  let data = req.body;
   let savedData = await userModel.create(data);
-  console.log(abcd.newAtribute);
-  xyz.send({ msg: savedData });
+  console.log(res.newAtribute);
+  res.send({ msg: savedData });
 };
 
 const loginUser = async function (req, res) {
@@ -41,22 +41,22 @@ const loginUser = async function (req, res) {
 };
 
 const getUserData = async function (req, res) {
-  let token = req.headers["x-Auth-token"];
-  if (!token) token = req.headers["x-auth-token"];
+//   let token = req.headers["x-Auth-token"];
+//   if (!token) token = req.headers["x-auth-token"];
 
-  //If no token is present in the request header return error
-  if (!token) return res.send({ status: false, msg: "token must be present" });
+//   //If no token is present in the request header return error
+//   if (!token) return res.send({ status: false, msg: "token must be present" });
 
-  console.log(token);
+//    console.log(token);
   
-  // If a token is present then decode the token with verify function
-  // verify takes two inputs:
-  // Input 1 is the token to be decoded
-  // Input 2 is the same secret with which the token was generated
-  // Check the value of the decoded token yourself
-  let decodedToken = jwt.verify(token, "functionup-radon");
-  if (!decodedToken)
-    return res.send({ status: false, msg: "token is invalid" });
+//   // If a token is present then decode the token with verify function
+//   // verify takes two inputs:
+//   // Input 1 is the token to be decoded
+//   // Input 2 is the same secret with which the token was generated
+//   // Check the value of the decoded token yourself
+//   let decodedToken = jwt.verify(token, "functionup-radon");
+//   if (!decodedToken)
+//     return res.send({ status: false, msg: "token is invalid" });
 
   let userId = req.params.userId;
   let userDetails = await userModel.findById(userId);
@@ -80,11 +80,27 @@ const updateUser = async function (req, res) {
   }
 
   let userData = req.body;
-  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
+  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData, {new:true});
   res.send({ status: updatedUser, data: updatedUser });
 };
 
+const deleteUser = async function (req, res) {
+
+      let userId = req.params.userId;
+      let user = await userModel.findById(userId);
+      //Return an error if no user with the given id exists in the db
+      if (!user) {
+        return res.send("No such user exists");
+      }
+    
+      let userData = req.body;
+      let deleteUser = await userModel.findOneAndUpdate({ _id: userId }, userData, {new:true});
+      res.send({ status: deleteUser, data: deleteUser });
+    };
+    
+    
 module.exports.createUser = createUser;
 module.exports.getUserData = getUserData;
 module.exports.updateUser = updateUser;
 module.exports.loginUser = loginUser;
+module.exports.deleteUser=deleteUser
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://subrat_400:4iQC1DP0ZqKInrD3@cluster0.h3xeivd.mongodb.net/subrat-DB", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
